21cbe0c09b8429643735628b4cd814407ad6dabf
SpaceCan <sheirtzler18@gmail.com>
2022-05-02 17:08:00 -0400

    Remove Demonstration Comments

--- ME643_Project_Part1.m
+++ ME643_Project_Part1.m
@@ -1,6 +1,5 @@
 %% Calculate Positions (Run this before plotting positions)
 clear;clc
-%grahams comment
 % WARNING: REQUIRES SYMBOLIC MATH TOOLBOX
 
 % All lengths are in meters
--- ME643_Project_Part2.m
+++ ME643_Project_Part2.m
@@ -2,5 +2,3 @@ clear;clc
 % Grab all the data variables from deliverable 1.
 % This saves an enormous amount of time.
 load('Deliverable_1_data.mat')
-% Add a comment
-% Nick's comment
